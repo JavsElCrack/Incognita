5dfd4bb927b187e8364d5624a66b4de7cf28c4a1
JavsElCrack <javscbc@gmail.com>
2024-09-13 12:51:27 -0500

    Fixed Computer Screen

--- Items/Interactables/PC.gd
+++ Items/Interactables/PC.gd
@@ -1,13 +1,14 @@
 extends Interactable
 var playercam
 @onready var pccam = $Camera3D
+@onready var screen = $Computer/Computer/RootNode/PC/Screen
 @onready var sub_viewport = $SubViewport
 var player
 var toggle = false
 var checkTransition = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_set_viewport_mat(screen,sub_viewport)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,5 +42,9 @@ func _on_interacted(body):
 			GameState.state["enableMinigame"] = false
 			sub_viewport.enableminigame = false
 			toggle = false
+func _set_viewport_mat(_display_mesh : MeshInstance3D, _sub_viewport : SubViewport, _surface_id : int = 0):
+	var _mat : StandardMaterial3D = StandardMaterial3D.new()
+	_mat.albedo_texture = _sub_viewport.get_texture()
+	_display_mesh.set_surface_override_material(_surface_id, _mat)
 
 
