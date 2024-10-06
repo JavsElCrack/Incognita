eaa1b4b030a16227fdc3256f194425f8729e2a4d
JavsElCrack <javscbc@gmail.com>
2024-10-05 23:13:47 -0500

    Room Prefab

--- Objects/door.gd
+++ Objects/door.gd
@@ -1,10 +1,17 @@
 extends Interactable
 @onready var animation_tree = $AnimationTree
+@onready var label = $MeshInstance3D/SubViewport/Label
+@onready var mesh_instance_3d = $MeshInstance3D
+@onready var sub_viewport = $MeshInstance3D/SubViewport
 @export var checkFlag = false
 @export var flagString : String
+@export var doorNumber : String
 var doorOpen = false
 
 
+func _ready():
+	label.text = doorNumber
+	_set_viewport_mat(mesh_instance_3d,sub_viewport)
 
 
 func _on_interacted(_body):
@@ -16,6 +23,11 @@ func _on_interacted(_body):
 	else:
 		toggleDoor()
 
+
+func _set_viewport_mat(_display_mesh : MeshInstance3D, _sub_viewport : SubViewport, _surface_id : int = 0):
+	var _mat : StandardMaterial3D = mesh_instance_3d.get_active_material(0)
+	_mat.albedo_texture = _sub_viewport.get_texture()
+	_display_mesh.set_surface_override_material(_surface_id, _mat)
 
 
 
