da515859975a97d66cbb027a2cbda72e93022618
JavsElCrack <69402362+JavsElCrack@users.noreply.github.com>
2024-10-08 10:28:19 -0500

    Main Menu done

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+func _on_play_button_down():
+	get_tree().change_scene_to_file("res://Scenes/Intro.tscn") # Replace with function body.
+
+
+func _on_credits_button_down():
+	pass
+
+
+func _on_quit_button_down():
+	get_tree().quit()
